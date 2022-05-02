112cc329e2e8cfeb6db25e8e33b723cf1c49e6fb
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2022-05-02 21:51:51 +0900

    Create 20220502.java

---
+++ 익조/20220502.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        s = s.replace("{", "");
+        s = s.replace("}", "");
+        String[] numbers = s.split(",");
+        for (String number : numbers) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+
+        int totalCount = map.size();
+        int[] answer = new int[totalCount];
+
+        for (String key : map.keySet()) {
+            answer[totalCount - map.get(key)] = Integer.parseInt(key);
+        }
+
+        return answer;
+    }
+}
