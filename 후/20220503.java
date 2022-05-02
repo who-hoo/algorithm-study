da0149eb4ff4ec349116507bc0395c8688d7b5c2
who-hoo <68011320+who-hoo@users.noreply.github.com>
2022-05-03 03:46:37 +0900

    Create 20220503.java

---
+++ 후/20220503.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int integer : arr) {
+            sb.append(integer).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
