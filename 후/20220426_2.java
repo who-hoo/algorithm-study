6b4b57ff5e9c5bd880e84cc6a2c1b29692fad647
who-hoo <68011320+who-hoo@users.noreply.github.com>
2022-05-04 10:52:56 +0900

    :twisted_rightwards_arrows: [후] 20220426 "프로그래머스 - 문자열 압축" 풀이 제출
    * Create 20220426_2.java
    
    * Update 20220426_2.java

---
+++ 후/20220426_2.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String compress(String s, int unit) {
+        StringBuilder result = new StringBuilder();
+        int cnt = 1;
+        String prev = s.substring(0, unit);
+        for (int i = unit; i < s.length(); i += unit) {
+            try {
+                String current = s.substring(i, i + unit);
+                if (current.equals(prev)) {
+                    cnt++;
+                } else {
+                    if (cnt == 1) {
+                        result.append(prev);
+                    } else {
+                        result.append(cnt).append(prev);
+                    }
+                    cnt = 1;
+                    prev = current;
+                }
+            } catch (IndexOutOfBoundsException e) {
+                if (cnt == 1) {
+                    result.append(prev);
+                } else {
+                    result.append(cnt).append(prev);
+                }
+                result.append(s.substring(i));
+                cnt = 1;
+                prev = "";
+            }
+        }
+        if (cnt == 1) {
+            result.append(prev);
+        } else {
+            result.append(cnt).append(prev);
+        }
+        return result.toString();
+    }
+
+    public int solution(String s) {
+        int min = s.length();
+        for (int i = 1; i <= s.length() / 2; i++) {
+            String compressed = compress(s, i);
+            min = Math.min(min, compressed.length());
+        }
+        return min;
+    }
+}
