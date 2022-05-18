f54397d224315bf93790fc2dd182be637365548e
함석호 <kalejuice02@gmail.com>
2022-05-18 22:02:03 +0900

    업뎃

--- 포키/20220518.java
+++ 포키/20220518.java
@@ -10,6 +10,12 @@ public class BOJ1238 {
 
         int[][] map = new int[n + 1][n + 1];
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = i==j ? 0 : 1_000_000;
+            }
+        }
+
         for (int i = 0; i < m; i++) {
             int s = sc.nextInt();
             int e = sc.nextInt();
