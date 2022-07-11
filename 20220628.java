de8797ea2df02e12376b8c9ca11b37e11528bc8f
donggi <devdonggilee@gmail.com>
2022-07-11 23:11:12 +0900

    Update 20220628.java

--- 20220628.java
+++ 20220628.java
@@ -1,50 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String[] strings = br.readLine().split(" ");
+        String[] strings = br.readLine().replace(",","").replace(";","").split(" ");
         String type = strings[0];
-        List<String> arr = new ArrayList<>();
 
         for (int i = 1; i < strings.length; i++) {
-            sb.append(type);
+            String result = type;
             String[] split = strings[i].split("");
-            boolean flag = true;
             for (int j = split.length - 1; j >= 0; j--) {
-                if (split[j].equals(",") || split[j].equals(";")) {
-                    continue;
-                }
 
-                if (split[j].equals("]")) {
-                    sb.append("[");
+                if (split[j].equals("*") || split[j].equals("&")) {
+                    result += split[j];
+                    continue;
+                } else if (split[j].equals("]")) {
+                    result += "[";
                     continue;
                 } else if (split[j].equals("[")) {
-                    sb.append("]");
+                    result += "]";
                     continue;
+                } else {
+                    result += " ";
+                    for (int k = 0; k <= j; k++) {
+                        result += split[k];
+                    }
+                    break;
                 }
-
-                if (97 <= split[j].charAt(0) && split[j].charAt(0) <= 122 && flag) {
-                    flag = false;
-                    sb.append(" ");
-                }
-                sb.append(split[j]);
-//                arr.add(split[j]);
             }
-
-            Collections.sort(arr, Collections.reverseOrder());
-            sb.append(arr);
-
-            sb.append(";").append("\n");
+            sb.append(result).append(";").append("\n");
         }
         System.out.println(sb);
     }
-
 }
