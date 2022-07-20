009a1041bf2857dd640cdab27b3a3ccbdbc3bcc2
Jaehong Choe <qnalsfj@gmail.com>
2022-07-21 00:40:07 +0900

    Update 20220719.java

--- 20220719.java
+++ 20220719.java
@@ -11,6 +11,7 @@ public class Main {
     private static int[] team1;
     private static int[] team2;
     private static boolean[] visited;
+    private static int answer = 123456789;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,37 +26,52 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < n / 2 + 1; i++) {
-            team1 = new int[n / 2];
-            team2 = new int[n / 2];
-            visited = new boolean[n];
-            permutation(i, 0);
-            calculate();
-        }
+        visited = new boolean[n];
+        permutation(0, 0);
 
+        System.out.println(answer);
     }
 
-    public static void permutation(int startNum, int depth) {
+     public static void permutation(int startNum, int depth) {
+        if (n / 2 == depth) {
+            calculate();
+            return;
+        }
         for (int i = startNum; i < n; i++) {
-            if (n / 2 == depth) return;
-            team1[depth] = i;
             visited[i] = true;
-            permutation(startNum + 1, depth + 1);
+            permutation(i + 1, depth + 1);
             visited[i] = false;
         }
     }
 
     private static void calculate() {
-        permutation2(0);
+        team1 = new int[n / 2];
+        team2 = new int[n / 2];
+        int idx1 = 0;
+        int idx2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == true) {
+                team1[idx1++] = i;
+            } else {
+                team2[idx2++] = i;
+            }
+        }
+
+        int score1 = 0;
+        int score2 = 0;
+        for (int i = 0; i < team1.length; i++) {
+            for (int j = 0; j < team1.length; j++) {
+                if (visited[i] && visited[j]) {
+                    score1 += board[i][j];
+                }
+                if (!visited[i] && !visited[j]) {
+                    score2 += board[i][j];
+                }
+            }
+        }
+
+        answer = Math.min(Math.abs(score1 - score2), answer);
     }
 
-    public static void permutation2(int depth) {
-        for (int i = 0; i < team1.length; i++) {
-            if (2 == depth) return; 
-            team1[depth] = i;
-            visited[i] = true;
-            permutation(startNum + 1, depth + 1);
-            visited[i] = false;
-        }
-    }
 }
