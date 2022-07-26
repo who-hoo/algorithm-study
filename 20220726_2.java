f61142057c2157c212819a845b4cfee5ae249330
Jaehong Choe <qnalsfj@gmail.com>
2022-07-26 23:52:23 +0900

    Create 20220726_2.java

---
+++ 20220726_2.java
@@ -0,0 +1,21 @@
+package beakjoon.main_1405;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+* 미친로봇
+* 골드V
+* */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] percentages = br.readLine().split(" ");
+
+        //단순할 확률 == 로봇이 같은 곳을 한 번보다 많이 이동하지 않을 확률.
+
+    }
+}
