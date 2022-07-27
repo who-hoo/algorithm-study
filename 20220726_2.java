865a5dd3e09b0664cd94e915e39f7ac4a064fee3
Jaehong Choe <qnalsfj@gmail.com>
2022-07-27 12:41:49 +0900

    Update 20220726_2.java

--- 20220726_2.java
+++ 20220726_2.java
@@ -1,7 +1,6 @@
 package beakjoon.main_1405;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -11,11 +10,43 @@ import java.io.InputStreamReader;
 * */
 public class Main {
 
+    private static int n;
+    private static double[] directions = new double[4];
+    private static int[] dx = {1, -1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
+    private static boolean[][] visited = new boolean[29][29];
+    private static double result = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] percentages = br.readLine().split(" ");
-
-        //단순할 확률 == 로봇이 같은 곳을 한 번보다 많이 이동하지 않을 확률.
-
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        for (int i = 0; i < 4; i++) {
+            directions[i] = Integer.parseInt(input[i + 1]) * 0.01;
+        }
+        visited[14][14] = true;
+        dfs(14, 14, 1.0);
+        System.out.println(result);
     }
+
+    public static void dfs(int x, int y, double probability) {
+        if (n == 0) {
+            result += probability; //여러 케이스로 성공할 수 있으니 성공할 때마다 계속 더해줌
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            //dx, dy의 요소를 directions의 동서남북 순서대로 배치하고
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            //동서남북 중 이동할 수 있는 확률이 존재할 때에만 dfs실행
+            if (!visited[nextY][nextX] && 0 < directions[i]) {
+                n--;
+                visited[nextY][nextX] = true;
+                dfs(nextX, nextY, probability * directions[i]);
+                visited[nextY][nextX] = false;
+                n++;
+            }
+        }
+    }
+    
 }
