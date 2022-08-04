b0361940e969babb26d5bbe31cc5f08316e89bf7
donggi <devdonggilee@gmail.com>
2022-08-04 11:59:24 +0900

    Update 20220802.java

--- 동기/20220802.java
+++ 동기/20220802.java
@@ -5,42 +5,42 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] board;
-    static boolean[][] visited;
-    static int n;
-    static int minResult = 10;
-    static int maxResult = -1;
-    static int minValue = 0;
-    static int maxValue = 0;
-    static int sum = 0;
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
-        board = new int[n][n];
-        visited = new boolean[n][n];
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][3];
+        int[][] dpMin = new int[n][3];
+        int[][] dpMax = new int[n][3];
+
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        visited[0][0] = true;
-        dfs(0);
-        System.out.println(maxValue + " " + minValue);
-        // 최대 점수와 최소 점수를 띄어서 출력
-    }
-
-    private static void dfs(int depth) {
-        // 첫번째 줄 각 칸에서 시작하여 마지막 줄까지 이동하면서 더해진 값을 각각 최대값, 최소값 변수에 값을 비교하여 저장
-        // 2중 for문으로 인덱스 체크해서 돌거면 visited는 필요가 없을 것 같기도하다
-        if (depth == n) {
-            return ;
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            board[i][0] = Integer.parseInt(st.nextToken());
+            board[i][1] = Integer.parseInt(st.nextToken());
+            board[i][2] = Integer.parseInt(st.nextToken());
         }
 
+        for (int i = 0; i < 3; i++) {
+            dpMin[0][i] = board[0][i];
+            dpMax[0][i] = board[0][i];
+        }
 
+        for (int i = 1; i < n; i++) {
+            dpMin[i][0] = board[i][0] + Math.min(dpMin[i - 1][0], dpMin[i - 1][1]);
+            dpMin[i][1] =
+                board[i][1] + Math.min(Math.min(dpMin[i - 1][0], dpMin[i - 1][1]), dpMin[i - 1][2]);
+            dpMin[i][2] = board[i][2] + Math.min(dpMin[i - 1][1], dpMin[i - 1][2]);
+
+            dpMax[i][0] = board[i][0] + Math.max(dpMax[i - 1][0], dpMax[i - 1][1]);
+            dpMax[i][1] =
+                board[i][1] + Math.max(Math.max(dpMax[i - 1][0], dpMax[i - 1][1]), dpMax[i - 1][2]);
+            dpMax[i][2] = board[i][2] + Math.max(dpMax[i - 1][1], dpMax[i - 1][2]);
+        }
+
+        int maxResult = Math.max(Math.max(dpMax[n - 1][0], dpMax[n - 1][1]), dpMax[n - 1][2]);
+        int minResult = Math.min(Math.min(dpMin[n - 1][0], dpMin[n - 1][1]), dpMin[n - 1][2]);
+
+        System.out.println(maxResult + " " + minResult);
     }
+
 }
