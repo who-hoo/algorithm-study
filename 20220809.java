6b3fb4d4dd72cc6daf16c4c592b660de7d2f3a10
Jaehong Choe <qnalsfj@gmail.com>
2022-08-10 00:10:48 +0900

    Update 20220809.java

--- 20220809.java
+++ 20220809.java
@@ -4,9 +4,14 @@ class Solution {
     
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
+    static int n;
+    static int m;
     
     public int solution(int[][] maps) {
+        n = maps.length;
+        m = maps[0].length;
         int answer = bfs(0, 0, maps);
+        
         return answer == 1 ? -1 : answer;
     }
     
@@ -20,13 +25,16 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 int nextX = xy[0] + dx[i];
                 int nextY = xy[1] + dy[i];
-                if (0 <= nextX && 0 <= nextY && nextX < 5 && nextY < 5 && maps[nextY][nextX] == 1) {
-                    maps[nextY][nextX] = maps[y][x] + 1;
-                    q.offer(new int[]{nextX, nextY});
+                
+                if (nextX == 0 && nextY == 0) continue;
+                
+                if (0 <= nextX && 0 <= nextY && nextX < m && nextY < n && maps[nextY][nextX] == 1) {
+                    maps[nextY][nextX] = maps[xy[1]][xy[0]] + 1;
+                    q.offer(new int[]{nextX, nextY});    
                 }
             }
         }
         
-        return maps[4][4];
+        return maps[n - 1][m - 1];
     }
 }
