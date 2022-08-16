90fc9f2f21cb7eaea9d4044a3bd12f05128d7d01
Jaehong Choe <qnalsfj@gmail.com>
2022-08-16 21:16:56 +0900

    Create 20220816.java

---
+++ 피오/20220816.java
@@ -0,0 +1,29 @@
+package beakjoon.main_1107;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+* 리모컨
+* 골드5
+* */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] n = br.readLine().split("");
+        int m = Integer.parseInt(br.readLine());
+
+        int[] broken = new int[m];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            broken[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(broken);
+
+        
+    }
+}
