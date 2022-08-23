0ab894fd1063bb18338d53a22943ad56458413a3
Jaehong Choe <qnalsfj@gmail.com>
2022-08-23 23:46:22 +0900

    Create 20220823.java

---
+++ 20220823.java
@@ -0,0 +1,19 @@
+package beakjoon.main_9019;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] nums = br.readLine().split(" ");
+            int num1 = Integer.parseInt(nums[0]);
+            int num2 = Integer.parseInt(nums[1]);
+        }
+    }
+}
