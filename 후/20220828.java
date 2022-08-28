a57b7fcb705b2695ad92b9e2be0b7c1cc90ee14a
who-hoo <68011320+who-hoo@users.noreply.github.com>
2022-08-28 22:52:50 +0900

    🔀 [후] 20220828 "프로그래머스 - 두 큐 합 같게 만들기" 풀이 제출
    테스트 케이스 25~27 실패(90/100)

---
+++ 후/20220828.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        for (int i = 0; i < queue1.length; i++) {
+            q1.offer(queue1[i]);
+            q2.offer(queue2[i]);
+        }
+
+        int answer = -1;
+        int count = 0;
+        int sum1 = q1.stream().mapToInt(Integer::intValue).sum();
+        int sum2 = q2.stream().mapToInt(Integer::intValue).sum();
+        while (true) {
+            if (count > queue1.length * 2 + 2) {
+                break;
+            }
+            if (sum1 == sum2) {
+                answer = count;
+                break;
+            } else if (sum1 > sum2) {
+                sum1 -= q1.peek();
+                sum2 += q1.peek();
+                q2.offer(q1.poll());
+            } else if (sum1 < sum2){
+                sum1 += q2.peek();
+                sum2 -= q2.peek();
+                q1.offer(q2.poll());
+            }
+            count++;
+        }
+
+        return answer;
+    }
+}
