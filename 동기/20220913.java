083e2c8075e6e6aa086351bb62b6b97f86345817
donggi <devdonggilee@gmail.com>
2022-09-14 10:00:53 +0900

    Update 20220913.java

--- 동기/20220913.java
+++ 동기/20220913.java
@@ -4,79 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+
     int answer = 0;
-    int[][] board;
-
-    public int solution(int n, int c, int r) {
-        board = makeBoard(n);
-        search(n, c, r);
-        return answer;
-    }
-
-    private void search(int n, int c, int r) {
-        // 한칸씩 이동할때마다 해당 인덱스에 +1
-
-        // 4등분 포인트 0부터 divideLength까지
-        // divideLength부터 board.length까지
-
-        int divideLength = 1;
-        int base = 2;
-        for (int i = 1; i < n; i++) {
-            divideLength *= base;
-        }
-
-        // c, r 에 따라서 순회할 네모 선택
-        // 3, 1
-        if (c > divideLength) {
-            answer += divideLength * divideLength * 2;
-            c -= divideLength;
-        }
-
-        if (r > divideLength) {
-            answer += divideLength * divideLength;
-            r -= divideLength;
-        }
-
-        // z모양 순회 로직
-
-        // 가로 +1, +3 순으로 증가
-        // 세로 +2, +6 순으로 증가
-        for (int i = 1; i <= c; i++) {
-            if (i % 2 == 0) {
-                answer += 6;
-            } else {
-                answer += 2;
-            }
-        }
-
-        for (int i = 1; i <= r; i++) {
-            if (i % 2 == 0) {
-                answer += 3;
-            } else {
-                answer += 1;
-            }
-        }
-    }
-
-    private int[][] makeBoard(int n) {
-        int base = 2;
-        int board = 2;
-        for (int i = 1; i < n; i++) {
-            board *= base;
-        }
-
-
-        return new int[board][board];
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
         Solution s = new Solution();
-        System.out.println(s.solution(n, c, r));
+        System.out.println(s.solution(n, r, c));
     }
 
+    public int solution(int n, int r, int c) {
+        n = (int) Math.pow(2, n);
+        search(n, r, c);
+        return answer;
+    }
+
+    public void search(int n, int r, int c) {
+        if (n == 1) {
+            return;
+        }
+
+        int size = n / 2;
+        // r, c가 size보다 작은 경우
+        if (r < size && c < size) {
+            search(size, r, c);
+        } else if (c >= size && r < size) {
+            answer += n * n / 4;
+            search(size, r, c - size);
+        } else if (r >= size && c < size) {
+            answer += n * n / 2;
+            search(size, r - size, c);
+        } else {
+            // r, c 둘 다 사이즈보다 클 경우
+            answer += n * n / 4 * 3;
+            search(size, r - size, c - size);
+        }
+    }
 }
