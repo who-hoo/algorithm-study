93474f414facbfdb2b0897b5042745c35263525b
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2022-09-16 02:23:33 +0900

    Update 20220623.java

--- 익조/20220623.java
+++ 익조/20220623.java
@@ -1,54 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 class Main {
 
-    static int n;
-    static boolean[] computers;
-    static int[][] network;
-    static boolean[][] visited;
+    static List<List<Integer>> network = new ArrayList<>();
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        computers = new boolean[n + 1];
-        network = new int[n + 1][n + 1];
-        visited = new boolean[n + 1][n + 1];
+
+        int n = Integer.parseInt(br.readLine()), m = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i <= n; i++) {
+            network.add(new ArrayList<>());
+        }
 
         StringTokenizer st;
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            network[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            network.get(start).add(end);
+            network.get(end).add(start);
         }
 
-        spread(1);
-
-        int result = 0;
-        for (int i = 2; i < n + 1; i++) {
-            if (computers[i]) {
-                result++;
-            }
-        }
+        boolean[] visited = new boolean[n + 1];
+        spread(1, visited);
 
         System.out.println(result);
     }
 
-    public static void spread(int start) {
-        if (!computers[start]) {
-            computers[start] = true;
-        }
+    private static void spread(int start, boolean[] visited) {
+        visited[start] = true;
 
-        for (int i = 1; i < n + 1; i++) {
-            if (network[start][i] == 1 && !visited[start][i]) {
-                visited[start][i] = true;
-                spread(i);
-            }
-            if (network[i][start] == 1 && !visited[i][start]) {
-                visited[i][start] = true;
-                spread(i);
+        for (int i = 0; i < network.get(start).size(); i++) {
+            int end = network.get(start).get(i);
+            if (!visited[end]) {
+                result++;
+                spread(end, visited);
             }
         }
     }
