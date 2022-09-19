5ec57596908fcde4e510986b3f98a125293a9b04
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2022-09-19 14:21:51 +0900

    Update 20220919.java

--- 익조/20220919.java
+++ 익조/20220919.java
@@ -57,7 +57,7 @@ class Solution {
             if (!bus.board(crew)) { // 크루가 버스에 탑승하지 못한 경우
                 while (busIndex + 1 < n) { // 다음에 탈 수 있는 버스가 있으면
                     if (buses.get(++busIndex).board(crew)) {
-                        break; // 이 크루 포함 이후의 크루들 모두 셔틀 버스를 영영 탈 수 없다.
+                        break;
                     }
                 }
             }
