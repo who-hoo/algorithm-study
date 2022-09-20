2268a2063371ec5b4e611750ccca11bd86903e07
who-hoo <68011320+who-hoo@users.noreply.github.com>
2022-09-20 23:13:33 +0900

    🔀 [후] 20220920 "프로그래머스 - n진수 게임" 풀이중.....

---
+++ 후/20220920_1.java
@@ -0,0 +1,9 @@
+public class Solution {
+
+    public String solution(int n, int t, int m, int p) {
+        // 진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p
+        // Integer.toString(n, radix) 사용해서 풀어보자 !
+        String answer = "";
+        return answer;
+    }
+}
