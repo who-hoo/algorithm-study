39cca1e6e6ce49182654eab83e9555bfa1654b31
who-hoo <68011320+who-hoo@users.noreply.github.com>
2022-09-20 23:15:50 +0900

    🔀 [후] 20220920 "프로그래머스 - 셔틀버스" 풀이중.....

---
+++ 후/20220920_2.java
@@ -0,0 +1,9 @@
+public class Solution {
+
+    public String solution(int n, int t, int m, String[] timetable) {
+        // 셔틀 운행 횟수 n, 셔틀 운행 간격 t,
+        // 한 셔틀에 탈 수 있는 최대 크루 수 m, 크루가 대기열에 도착하는 시간 timetable
+        String answer = "";
+        return answer;
+    }
+}
