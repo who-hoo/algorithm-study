ac44b1f8b2221ad5a9798dbd4f4591375aeeb7fb
donggi <devdonggilee@gmail.com>
2022-09-27 15:21:40 +0900

    Update 20220920.java

--- 동기/20220920.java
+++ 동기/20220920.java
@@ -1,42 +1,43 @@
-package com.example.algover2.programmers.lv2.셔틀버스;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Solution {
 
     public String solution(int n, int t, int m, String[] timetable) {
-        String answer = "";
-        boolean[] times = new boolean[1500];
-        int lastTime = 540 + (n - 1) * t;
-
-        // 이것은 사람들이 탈 시간 아으
-        // 분으로 바꿔서 오름차순 ArrayList
-        List<Integer> arr = new ArrayList<>();
-
-        for (String s : timetable) {
-            String[] split = s.split(":");
-            int time = Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
-//            times[time] = true;
-            arr.add(time);
+        Arrays.sort(timetable);
+        PriorityQueue<Integer> minuteTimeTable = new PriorityQueue<>();
+        for (String time : timetable) {
+            String[] split = time.split(":");
+            int minute = Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
+            minuteTimeTable.add(minute);
         }
-        Collections.sort(arr);
-        // 480 481 482 483
-        // 마지막 셔틀 버스에 탄 사람에 대해 알고싶다.
-        int lastPerson = arr.get(arr.size() - 1); // 483
-        int lastBus = 0;
-        for (int i = 540; i < n - 1; i += t) {
-            lastBus = i;
-            for (int j = 0; j < m; j++) {
-                Integer integer = arr.get(j);
-                if (lastBus >= integer) {
-                    arr.remove(integer);
+
+        int currentBusTime = 540;
+        int answer = 0;
+        int index = 0;
+        
+        for (int i = 0; i < n; i++) {
+            index = 0;
+            while (!minuteTimeTable.isEmpty()) {
+                int crew = minuteTimeTable.poll();
+                if (crew <= currentBusTime && index < m) {
+                    index++;
+                    answer = crew - 1;
+                } else {
+                    minuteTimeTable.offer(crew);
+                    break;
                 }
             }
+            currentBusTime += t;
         }
 
+        if (index < m) {
+            answer = currentBusTime - t;
+        }
 
-        return answer;
+        int hour = answer / 60;
+        int minute = answer % 60;
+        return (hour < 10 ? "0" + hour : hour + "") + ":" +
+            (minute < 10 ? "0" + minute : minute + "");
     }
 }
