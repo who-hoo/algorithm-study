00a4f5ddc47150946e84b11f0b8113dcbafb2e55
who-hoo <68011320+who-hoo@users.noreply.github.com>
2022-10-04 00:39:04 +0900

    :recycle: 메서드 리팩토링

--- 후/20221004_1.java
+++ 후/20221004_1.java
@@ -6,15 +6,22 @@ class Solution {
 
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         initSellers(enroll, referral);
-        for (int i = 0; i < seller.length; i++) {
-            sellers.get(seller[i]).distributeProfit(priceOfToothBrush * amount[i]);
-        }
+        distribute(seller, amount);
+        return getTotalProfits(enroll);
+    }
 
+    private int[] getTotalProfits(String[] enroll) {
         int[] answer = new int[enroll.length];
         for (int i = 0; i < enroll.length; i++) {
             answer[i] = sellers.get(enroll[i]).profit;
         }
         return answer;
+    }
+
+    private void distribute(String[] seller, int[] amount) {
+        for (int i = 0; i < seller.length; i++) {
+            sellers.get(seller[i]).distributeProfit(priceOfToothBrush * amount[i]);
+        }
     }
 
     private void initSellers(String[] enroll, String[] referral) {
