36f13b2435784c1792d6146066ddde22c1ef45ba
SeokHo-Ham <kalejuice02@gmail.com>
2022-10-05 00:00:13 +0900

    회전초밥 풀이중

---
+++ 포키/20221004.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2531 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int d = Integer.parseInt(input[1]);
+        int k = Integer.parseInt(input[2]);
+        int c = Integer.parseInt(input[3]);
+
+    }
+
+}
