eca9a4312248b0c579c15916a234d1ffc92515e6
who-hoo <68011320+who-hoo@users.noreply.github.com>
2022-10-10 16:26:49 +0900

    🔀 [후] 20221004 "프로그래머스 - 다단계 칫솔 판매" 풀이 제출 (#486)
    * 🔀 [후] 20221004 "프로그래머스 - 다단계 칫솔 판매" 풀이 제출
    
    * :recycle: 메서드 리팩토링

---
+++ 후/20221004_1.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+
+class Solution {
+    final int priceOfToothBrush = 100;
+    HashMap<String, Seller> sellers = new HashMap<>();
+
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        initSellers(enroll, referral);
+        distribute(seller, amount);
+        return getTotalProfits(enroll);
+    }
+
+    private int[] getTotalProfits(String[] enroll) {
+        int[] answer = new int[enroll.length];
+        for (int i = 0; i < enroll.length; i++) {
+            answer[i] = sellers.get(enroll[i]).profit;
+        }
+        return answer;
+    }
+
+    private void distribute(String[] seller, int[] amount) {
+        for (int i = 0; i < seller.length; i++) {
+            sellers.get(seller[i]).distributeProfit(priceOfToothBrush * amount[i]);
+        }
+    }
+
+    private void initSellers(String[] enroll, String[] referral) {
+        for (String name : enroll) {
+            sellers.put(name, new Seller(name));
+        }
+        for (int i = 0; i < referral.length; i++) {
+            sellers.get(enroll[i]).referee = sellers.get(referral[i]);
+        }
+    }
+
+    class Seller {
+
+        String name;
+        Seller referee;
+        int profit;
+
+        Seller(String name) {
+            this.name = name;
+        }
+
+        void distributeProfit(int profit) {
+            int commission = (int) (profit * 0.1);
+            this.profit += profit - commission;
+            if (referee != null) {
+                referee.distributeProfit(commission);
+            }
+        }
+    }
+}
