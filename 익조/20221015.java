133ad13d6c60d2466c3ea3535801415d952e0241
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2022-10-15 00:23:43 +0900

    Create 20221015.java

---
+++ 익조/20221015.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken()) + arr[i][j - 1];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int startRow = Integer.parseInt(st.nextToken());
+            int startCol = Integer.parseInt(st.nextToken());
+            int endRow = Integer.parseInt(st.nextToken());
+            int endCol = Integer.parseInt(st.nextToken());
+
+            int sum = 0;
+            for (int j = startRow; j <= endRow; j++) {
+                sum += arr[j][endCol] - arr[j][startCol - 1];
+            }
+
+            bw.write(String.valueOf(sum));
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
