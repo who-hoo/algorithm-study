34450b11b3f6f1d1cb3162f8f9025c0f26443eaf
donggi <devdonggilee@gmail.com>
2022-11-23 12:41:11 +0900

    Update 20221122.java

--- 동기/20221122.java
+++ 동기/20221122.java
@@ -3,7 +3,41 @@ class Solution {
     int MOD = 20170805;
 
     public int solution (int m, int n, int[][] cityMap) {
-        int answer = 0;
-        return answer;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+
+        if (cityMap[1][0] != 1) {
+            dp[1][0] = 1;
+        } else {
+            dp[1][0] = 0;
+        }
+
+        if (cityMap[0][1] != 1) {
+            dp[0][1] = 1;
+        } else {
+            dp[0][1] = 0;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (cityMap[i - 1][j] == 1) {
+                    continue;
+                }
+
+                if (cityMap[i][j - 1] == 1) {
+                    continue;
+                }
+
+                if (cityMap[i][j] == 2) {
+                    dp[i][j] += (dp[i][j - 1] + dp[i - 1][j]) % MOD;
+                }
+
+                if (cityMap[i][j] == 0) {
+                    dp[i][j] += (dp[i][j - 1] + dp[i - 1][j]) % MOD;
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1] % MOD;
     }
 }
