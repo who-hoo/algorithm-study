78d540b80721ee2a795d197f5364e4cacd0c63f1
SeokHo-Ham <kalejuice02@gmail.com>
2022-11-29 22:37:08 +0900

    연산자 끼워넣기

---
+++ 포키/20221129.java
@@ -0,0 +1,12 @@
+public class BOJ14888 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] nums = br.readLine().split(" ");
+        String[] commands = br.readLine().split(" ");
+        
+
+    }
+}
