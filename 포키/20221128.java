50d832504c36317adf90fd5c987e9de32c7e2269
SeokHo-Ham <kalejuice02@gmail.com>
2022-11-29 22:33:45 +0900

    뱀과 사다리

---
+++ 포키/20221128.java
@@ -0,0 +1,13 @@
+class BOJ16928 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+
+    }
+
+}
