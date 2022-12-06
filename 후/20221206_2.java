93d0b98e8dd73ab9d31b98d65aba4d982de407cb
who-hoo <68011320+who-hoo@users.noreply.github.com>
2022-12-06 23:44:27 +0900

    🔀 [후] 20221206 "백준 - BOJ거리" 풀이 제출

---
+++ 후/20221206_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int N = scanner.nextInt();
+        String blocks = scanner.nextLine();
+        scanner.close();
+
+        //TODO: BOJ를 외치면서 점프~!
+    }
+}
